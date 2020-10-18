e6b868fa8904aa5e0909f26f45d6b43b71f996de
FarzanehEntezari <farzaneh0343@gmail.com>
2020-10-18 16:54:33 -0400

    INP-fitting incrementally while subscribing to IMU messages!

--- fault_detection/scripts/fault_detection_node.py
+++ fault_detection/scripts/fault_detection_node.py
@@ -2,14 +2,26 @@
 
 
 import rospy
+import time
 from sensor_msgs.msg import Imu
+from sklearn.neighbors import KernelDensity
 
-def callback(msg):
-    rospy.loginfo("Got the message!")
+class Gaussian_Estimation():
+    def __init__ (self):
+        self.kde = KernelDensity(kernel='gaussian' , bandwidth = 0.2)
+
+    def callback(self , msg):
+        rospy.loginfo("Got the message!")
+        #rospy.loginfo(msg.linear_acceleration.x)
+        self.kde.partial_fit(msg.linear_acceleration.x)
+        if time.time % 30 == 0 :
+            print(self.kde.get_params(deep=False) )
+
 
 def main():
     rospy.init_node('fault_detection', anonymous = True)
-    rospy.Subscriber('imu' , Imu , callback)
+    GK = Gaussian_Estimation()
+    rospy.Subscriber('imu' , Imu , GK.callback)
     rospy.spin()
 
 if  __name__ =="__main__" :
